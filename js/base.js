4d5a125ab4e4c146aa1bf4be24f6a8de333b63f0
799378666@qq.com <799378666@qq.com>
2017-02-17 06:55:14 +0800

    ...

--- js/base.js
+++ js/base.js
@@ -4,7 +4,7 @@
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "https://hm.baidu.com/hm.js?75badb595f989fbd7700b3ea4af93f03";
+    hm.src = "https://hm.baidu.com/hm.js?cbdbfcbfec0d354302fc398db2724563";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
